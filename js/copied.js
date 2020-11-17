047f06681bc414dc6e96ad4a44528024d1698903
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 13:10:25 -0300

    Nuevas funciones js para copiar datos de contacto

---
+++ js/copied.js
@@ -0,0 +1,20 @@
+function lblMail(){
+    var b = document.getElementById("boton-mail");
+    b.setAttribute('data-original-title', 'Copiado');
+    $(b).tooltip('show');
+    b.setAttribute('data-original-title', 'Copiar');
+}
+
+function lblTelefono(){
+	var b = document.getElementById("boton-telefono");
+    b.setAttribute('data-original-title', 'Copiado');
+    $(b).tooltip('show');
+    b.setAttribute('data-original-title', 'Copiar');
+}
+
+function lblUbicacion(){
+	var b = document.getElementById("boton-ubicacion");
+    b.setAttribute('data-original-title', 'Copiado');
+    $(b).tooltip('show');
+    b.setAttribute('data-original-title', 'Copiar');
+}
